cbe6de3d08cd6a6a87325141a64ba7977b834d20
jycx50 <g4ua1la4@jayc4.anonaddy.com>
2022-08-30 12:10:54 +0800

    Add external links & check some URLs

--- app/viewbird.py
+++ app/viewbird.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, g
 import sqlite3
+import requests
 
 viewbird_bp = Blueprint('viewbird_bp', __name__, template_folder='templates/viewbird')
 
@@ -21,8 +22,38 @@ def viewbird(birdNum):
             birdInfo[12] = int(birdInfo[12])
         if birdInfo[13] and birdInfo[13] % 1 == 0:
             birdInfo[13] = int(birdInfo[13])
+        
+        engNamel = list(birdInfo[2])
+        invalidChar = "\'"
+        while invalidChar in engNamel:
+            engNamel.remove(invalidChar)
+        sgBirdsName = "".join(engNamel)
+
+        sciNamel = list(birdInfo[3])
+        invalidChar = " "
+        while invalidChar in sciNamel:
+            sciNamel[sciNamel.index(invalidChar)] = "-"
+        xenoName = "".join(sciNamel)
+
+        sgBirdsLink = f"https://singaporebirds.com/species/{sgBirdsName}"
+        iucnLink = f"https://apiv3.iucnredlist.org/api/v3/website/{birdInfo[3].lower()}"
+        xenoLink = f"https://xeno-canto.org/species/{xenoName}"
+        
+        links = {"More Information (Singapore Birds Project)":sgBirdsLink,"Conservation Status (IUCN Red List)":iucnLink,"Sound Recordings (xeno-canto)":xenoLink}
+        for link in links:
+            try:
+                if link == "Conservation Status (IUCN Red List)":
+                    continue # IUCN api takes too long to respond
+                else:
+                    resp = requests.get(links[link],timeout=0.01)
+                    if response.status_code != requests.codes.ok:
+                        links.pop(link)
+            except:
+                continue
+
         localStat = {"I":"Introduced","M":"Migrant","R":"Resident","Va":"Vagrant","Vi":"Visitor","E":"Extirpated"}
-        return render_template("bird_info.html",birdInfo=birdInfo,localStat=localStat)
+
+        return render_template("bird_info.html",birdInfo=birdInfo,localStat=localStat,links=links)
     else:
         return render_template("bird_not_found.html"), 404
 
