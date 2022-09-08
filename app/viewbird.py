dab3bb87e71e0f53189ce2e42526f9698011955e
jycx50 <g4ua1la4@jayc4.anonaddy.com>
2022-09-08 12:09:52 +0800

    Use sqlite3.Row

--- app/viewbird.py
+++ app/viewbird.py
@@ -9,41 +9,36 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
     return db
 
 @viewbird_bp.route("/<int:birdNum>",methods=["GET"])
 def viewbird(birdNum):
     cur = get_db().cursor()
-    cur.execute("SELECT * FROM Birds WHERE Num = ?",(birdNum,))
+    cur.execute("SELECT Family, Num, EngName, SciName, MalName, ChName, RarityStatus, LocalStatus, Identification, Habitat, Behaviour, MinSize, MaxSize FROM Birds WHERE Num = ?",(birdNum,))
     birdInfo = cur.fetchone()
     if birdInfo:
-        birdInfo = list(birdInfo[:-3])
-        if birdInfo[11] and birdInfo[11] % 1 == 0:
-            birdInfo[11] = int(birdInfo[11])
-        if birdInfo[12] and birdInfo[12] % 1 == 0:
-            birdInfo[12] = int(birdInfo[12])
-        
-        engNamel = list(birdInfo[2])
+        engNamel = list(birdInfo["EngName"])
         invalidChar = "\'"
         while invalidChar in engNamel:
             engNamel.remove(invalidChar)
         sgBirdsName = "".join(engNamel)
 
-        sciNamel = list(birdInfo[3])
+        sciNamel = list(birdInfo["SciName"])
         invalidChar = " "
         while invalidChar in sciNamel:
             sciNamel[sciNamel.index(invalidChar)] = "-"
         xenoName = "".join(sciNamel)
 
         sgBirdsLink = f"https://singaporebirds.com/species/{sgBirdsName}"
-        iucnLink = f"https://apiv3.iucnredlist.org/api/v3/website/{birdInfo[3].lower()}"
+        iucnLink = f"https://apiv3.iucnredlist.org/api/v3/website/{birdInfo['SciName'].lower()}"
         xenoLink = f"https://xeno-canto.org/species/{xenoName}"
         
-        links = {"More Information (Singapore Birds Project)":sgBirdsLink,"Conservation Status (IUCN Red List)":iucnLink,"Sound Recordings (xeno-canto)":xenoLink}
+        links = {"More Information & Photos (Singapore Birds Project)":sgBirdsLink,"Conservation Status (IUCN Red List)":iucnLink,"Sound Recordings (xeno-canto)":xenoLink}
         for link in links:
             try:
                 if link == "Conservation Status (IUCN Red List)":
-                    continue # IUCN api takes too long to respond
+                    continue # IUCN API takes too long to respond
                 else:
                     resp = requests.get(links[link],timeout=0.01)
                     if response.status_code != requests.codes.ok:
